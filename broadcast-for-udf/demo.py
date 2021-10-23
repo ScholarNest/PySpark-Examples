67c0aa85b7269dc7c180436fb90247c845d309a3
ScholarNest <86905722+ScholarNest@users.noreply.github.com>
2021-10-23 12:56:30 +0530

    Create demo.py

---
+++ broadcast-for-udf/demo.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+def my_func(code):
+    # return prdCode.get(code)
+    return bdData.value.get(code)
+
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Demo") \
+        .master("local[3]") \
+        .getOrCreate()
+
+    prdCode = {"01056": "Product 1", "98312": "Product 2", "02845": "Product 3"}
+    bdData = spark.sparkContext.broadcast(prdCode)
+
+    data_list = [("98312", "2021-01-01", "1200", "01"),
+                 ("01056", "2021-01-02", "2345", "01"),
+                 ("98312", "2021-02-03", "1200", "02"),
+                 ("01056", "2021-02-04", "2345", "02"),
+                 ("02845", "2021-02-05", "9812", "02")]
+
+    df = spark.createDataFrame(data_list) \
+        .toDF("code", "order_date", "price", "qty")
+
+    spark.udf.register("my_udf", my_func, StringType())
+
+    df.withColumn("Product", expr("my_udf(code)")) \
+        .show()
+
+    input()
