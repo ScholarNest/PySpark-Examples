da5150e25ad3d9a2b7ff7e8c62b676708dcd56b5
ScholarNest <86905722+ScholarNest@users.noreply.github.com>
2021-10-25 17:37:46 +0530

    Update demo.py

--- dataframe-hints/demo.py
+++ dataframe-hints/demo.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     flight_time_df1 = spark.read.json("data/d1/")
     flight_time_df2 = spark.read.json("data/d2/")
 
+    # join_df = flight_time_df1.join(flight_time_df2.hint("broadcast"), "id", "inner")
+    
     join_df = flight_time_df1.join(broadcast(flight_time_df2), "id", "inner") \
         .hint("COALESCE", 5)
 
