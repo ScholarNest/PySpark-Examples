f81c4c43d7c2f8eef04f3d5286fe4a04d297a0b5
ScholarNest <86905722+ScholarNest@users.noreply.github.com>
2021-10-25 12:46:08 +0530

    Create demo.py

---
+++ dataframe-hints/demo.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .appName("Demo") \
+        .master("local[3]") \
+        .getOrCreate()
+
+    flight_time_df1 = spark.read.json("data/d1/")
+    flight_time_df2 = spark.read.json("data/d2/")
+
+    join_df = flight_time_df1.join(broadcast(flight_time_df2), "id", "inner") \
+        .hint("COALESCE", 5)
+
+    join_df.show()
+    print("Number of Output Partitions:" + str(join_df.rdd.getNumPartitions()))
